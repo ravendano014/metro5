94cc23b61e5e30c4e7099caf5da87472dc6fd054
Sergey Pimenov <sergey@pimenov.com.ua>
2021-01-13 20:45:55 +0200

    add rollup config to cake

---
+++ packages/cake/config/banner.js
@@ -0,0 +1,10 @@
+import pack from '../package.json';
+
+export default `/*! 
+ * CakeJS - string library ${pack.version}
+ * https://picmenov.com.ua
+ *
+ * Copyright 2020-2021 Serhii Pimenov
+ * Released under the MIT license
+ */
+`;
