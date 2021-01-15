ab1f9f5f3d9e229ec72a5435b8c8735085c05122
Sergey Pimenov <sergey@pimenov.com.ua>
2021-01-16 01:43:58 +0200

    added rollup config for query

--- packages/query/src/class.js
+++ packages/query/src/class.js
@@ -2,9 +2,14 @@ export default class Query extends Array {
     constructor(sel, ctx) {
         super();
 
-
+        this._selector = sel
+        this._context = ctx
     }
 }
+
+Query.use = function(...mixins){
+    Object.assign(Query.prototype, ...mixins);
+};
 
 export const $ = function(sel, ctx){
     return new Query(sel, ctx)
